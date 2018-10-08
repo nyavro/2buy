e0e27642392c772290adccf71e448c753daa9e07
nyavro <e.nyavro@gmail.com>
2018-10-08 23:21:10 +0700

    Formats

--- rest/src/main/scala/com/nyavro/tobuy/util/CustomFormats.scala
+++ rest/src/main/scala/com/nyavro/tobuy/util/CustomFormats.scala
@@ -1,23 +1,24 @@
 package com.nyavro.tobuy.util
 
 import java.sql.Timestamp
+import java.util.Date
 
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 import spray.json.{DeserializationException, JsString, JsValue, JsonFormat}
 
 trait CustomFormats {
-  implicit object TimestampFormat extends JsonFormat[Timestamp] {
+  implicit object TimestampFormat extends JsonFormat[Date] {
     val format = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
 
-    def write(timestamp: Timestamp) = {
+    def write(timestamp: Date) = {
       val dt = new DateTime(timestamp.getTime)
       JsString(format.print(dt))
     }
     def read(json: JsValue) = json match {
       case JsString(time) => {
         val dt = format.parseDateTime(time)
-        new Timestamp(dt.getMillis)
+        new Date(dt.getMillis)
       }
       case _ => throw new DeserializationException("Date expected")
     }
